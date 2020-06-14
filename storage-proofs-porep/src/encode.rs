e6aa69c9509c1fabb9aaca6671ed1be289609dc3
davidad <davidad@alum.mit.edu>
2020-06-14 02:39:59 +0000

    Just for beauty...

---
+++ storage-proofs-porep/src/encode.rs
@@ -0,0 +1,19 @@
+use bellperson::bls::Fr;
+use ff::Field;
+use filecoin_hashers::Domain;
+
+pub fn encode<T: Domain>(key: T, value: T) -> T {
+    let mut result: Fr = value.into();
+    let key: Fr = key.into();
+
+    result.add_assign(&key);
+    result.into()
+}
+
+pub fn decode<T: Domain>(key: T, value: T) -> T {
+    let mut result: Fr = value.into();
+    let key: Fr = key.into();
+
+    result.sub_assign(&key);
+    result.into()
+}
