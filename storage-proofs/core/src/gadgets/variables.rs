758a90d9e37946a494bda16d2848b81754803df2
arajasek94 <arajasek94@gmail.com>
2020-07-20 00:22:59 +0000

    call to action on stories closes #74 and #83

---
+++ storage-proofs/core/src/gadgets/variables.rs
@@ -0,0 +1,55 @@
+use std::fmt;
+
+use anyhow::Result;
+
+use bellperson::gadgets::num::AllocatedNum;
+use bellperson::{ConstraintSystem, SynthesisError};
+use paired::Engine;
+
+/// Root represents a root commitment which may be either a raw value or an already-allocated number.
+/// This allows subcomponents to depend on roots which may optionally be shared with their parent
+/// or sibling components.
+#[derive(Clone)]
+pub enum Root<E: Engine> {
+    Var(AllocatedNum<E>),
+    Val(Option<E::Fr>),
+}
+
+impl<E: Engine> fmt::Debug for Root<E> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Root::Var(num) => write!(f, "Root::Var({:?})", num.get_value()),
+            Root::Val(val) => write!(f, "Root::Val({:?})", val),
+        }
+    }
+}
+
+impl<E: Engine> Root<E> {
+    pub fn from_allocated<CS: ConstraintSystem<E>>(allocated: AllocatedNum<E>) -> Self {
+        Root::Var(allocated)
+    }
+
+    pub fn allocated<CS: ConstraintSystem<E>>(
+        &self,
+        cs: CS,
+    ) -> Result<AllocatedNum<E>, SynthesisError> {
+        match self {
+            Root::Var(allocated) => Ok(allocated.clone()),
+            Root::Val(fr) => {
+                AllocatedNum::alloc(cs, || fr.ok_or_else(|| SynthesisError::AssignmentMissing))
+            }
+        }
+    }
+
+    pub fn var<CS: ConstraintSystem<E>>(cs: CS, fr: E::Fr) -> Result<Self> {
+        Ok(Root::Var(AllocatedNum::alloc(cs, || Ok(fr))?))
+    }
+
+    pub fn is_some(&self) -> bool {
+        match self {
+            Root::Var(_) => true,
+            Root::Val(Some(_)) => true,
+            Root::Val(None) => false,
+        }
+    }
+}
