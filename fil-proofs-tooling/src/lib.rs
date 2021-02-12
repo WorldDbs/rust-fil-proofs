c5d8c8b653070b6da84920c99a603465b12b7884
ligi <ligi@ligi.de>
2021-02-12 08:36:30 +0000

    separate compare & resuggest commands

--- fil-proofs-tooling/src/lib.rs
+++ fil-proofs-tooling/src/lib.rs
@@ -1,3 +1,7 @@
+#![deny(clippy::all, clippy::perf, clippy::correctness, rust_2018_idioms)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::needless_collect)]
+
 pub mod measure;
 pub mod metadata;
 pub mod shared;
