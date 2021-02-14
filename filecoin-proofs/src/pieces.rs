9688f5b1a77ee27fee8717ae3e86ed32592d8809
hugomrdias <hugomrdias@gmail.com>
2021-02-14 02:37:38 +0000

    Making changes to the README according to the recent API-change.

--- filecoin-proofs/src/pieces.rs
+++ filecoin-proofs/src/pieces.rs
@@ -1,21 +1,26 @@
+use std::cmp::min;
 use std::collections::HashMap;
-use std::io::Read;
-use std::io::{self, Cursor};
+use std::io::{self, Cursor, Read};
 use std::iter::Iterator;
 use std::sync::Mutex;
 
 use anyhow::{ensure, Context, Result};
+use filecoin_hashers::{HashFunction, Hasher};
+use fr32::Fr32Reader;
 use lazy_static::lazy_static;
 use log::info;
-use storage_proofs::hasher::{HashFunction, Hasher};
-use storage_proofs::util::NODE_SIZE;
+use storage_proofs_core::util::NODE_SIZE;
 
-use crate::constants::{
-    DefaultPieceHasher,
-    MINIMUM_RESERVED_BYTES_FOR_PIECE_IN_FULLY_ALIGNED_SECTOR as MINIMUM_PIECE_SIZE,
-};
-use crate::types::{
-    Commitment, PaddedBytesAmount, PieceInfo, SectorSize, UnpaddedByteIndex, UnpaddedBytesAmount,
+use crate::{
+    commitment_reader::CommitmentReader,
+    constants::{
+        DefaultPieceHasher,
+        MINIMUM_RESERVED_BYTES_FOR_PIECE_IN_FULLY_ALIGNED_SECTOR as MINIMUM_PIECE_SIZE,
+    },
+    types::{
+        Commitment, PaddedBytesAmount, PieceInfo, SectorSize, UnpaddedByteIndex,
+        UnpaddedBytesAmount,
+    },
 };
 
 /// Verify that the provided `piece_infos` and `comm_d` match.
@@ -32,11 +37,9 @@ pub fn verify_pieces(
 lazy_static! {
     static ref COMMITMENTS: Mutex<HashMap<SectorSize, Commitment>> = Mutex::new(HashMap::new());
 }
-use crate::commitment_reader::CommitmentReader;
-use crate::fr32_reader::Fr32Reader;
 
 #[derive(Debug, Clone)]
-struct EmptySource {
+pub struct EmptySource {
     size: usize,
 }
 
@@ -48,7 +51,7 @@ impl EmptySource {
 
 impl Read for EmptySource {
     fn read(&mut self, target: &mut [u8]) -> io::Result<usize> {
-        let to_read = std::cmp::min(self.size, target.len());
+        let to_read = min(self.size, target.len());
         self.size -= to_read;
         for val in target {
             *val = 0;
@@ -59,13 +62,14 @@ impl Read for EmptySource {
 }
 
 fn empty_comm_d(sector_size: SectorSize) -> Commitment {
-    let map = &mut *COMMITMENTS.lock().unwrap();
+    let map = &mut *COMMITMENTS.lock().expect("COMMITMENTS poisoned");
 
     *map.entry(sector_size).or_insert_with(|| {
         let size: UnpaddedBytesAmount = sector_size.into();
         let fr32_reader = Fr32Reader::new(EmptySource::new(size.into()));
         let mut commitment_reader = CommitmentReader::new(fr32_reader);
-        io::copy(&mut commitment_reader, &mut io::sink()).unwrap();
+        io::copy(&mut commitment_reader, &mut io::sink())
+            .expect("failed to copy commitment to sink");
 
         let mut comm = [0u8; 32];
         comm.copy_from_slice(
@@ -104,7 +108,10 @@ pub fn compute_comm_d(sector_size: SectorSize, piece_infos: &[PieceInfo]) -> Res
 
     let mut stack = Stack::new();
 
-    let first = piece_infos.first().unwrap().clone();
+    let first = piece_infos
+        .first()
+        .expect("unreachable: !is_empty()")
+        .clone();
     ensure!(
         u64::from(PaddedBytesAmount::from(first.size)).is_power_of_two(),
         "Piece size ({:?}) must be a power of 2.",
@@ -142,31 +149,31 @@ struct Stack(Vec<PieceInfo>);
 
 impl Stack {
     /// Creates a new stack.
-    pub fn new() -> Self {
+    fn new() -> Self {
         Stack(Vec::new())
     }
 
     /// Pushes a single element onto the stack.
-    pub fn shift(&mut self, el: PieceInfo) {
+    fn shift(&mut self, el: PieceInfo) {
         self.0.push(el)
     }
 
     /// Look at the last element of the stack.
-    pub fn peek(&self) -> &PieceInfo {
+    fn peek(&self) -> &PieceInfo {
         &self.0[self.0.len() - 1]
     }
 
     /// Look at the second to last element of the stack.
-    pub fn peek2(&self) -> &PieceInfo {
+    fn peek2(&self) -> &PieceInfo {
         &self.0[self.0.len() - 2]
     }
 
     /// Pop the last element of the stack.
-    pub fn pop(&mut self) -> Result<PieceInfo> {
+    fn pop(&mut self) -> Result<PieceInfo> {
         self.0.pop().context("empty stack popped")
     }
 
-    pub fn reduce1(&mut self) -> Result<bool> {
+    fn reduce1(&mut self) -> Result<bool> {
         if self.len() < 2 {
             return Ok(false);
         }
@@ -182,23 +189,23 @@ impl Stack {
         Ok(false)
     }
 
-    pub fn reduce(&mut self) -> Result<()> {
+    fn reduce(&mut self) -> Result<()> {
         while self.reduce1()? {}
         Ok(())
     }
 
-    pub fn shift_reduce(&mut self, piece: PieceInfo) -> Result<()> {
+    fn shift_reduce(&mut self, piece: PieceInfo) -> Result<()> {
         self.shift(piece);
         self.reduce()
     }
 
-    pub fn len(&self) -> usize {
+    fn len(&self) -> usize {
         self.0.len()
     }
 }
 
 /// Create a padding `PieceInfo` of size `size`.
-fn zero_padding(size: UnpaddedBytesAmount) -> Result<PieceInfo> {
+pub fn zero_padding(size: UnpaddedBytesAmount) -> Result<PieceInfo> {
     let padded_size: PaddedBytesAmount = size.into();
     let mut commitment = [0u8; 32];
 
@@ -236,7 +243,7 @@ fn join_piece_infos(mut left: PieceInfo, right: PieceInfo) -> Result<PieceInfo> 
     Ok(left)
 }
 
-pub(crate) fn piece_hash(a: &[u8], b: &[u8]) -> <DefaultPieceHasher as Hasher>::Domain {
+pub fn piece_hash(a: &[u8], b: &[u8]) -> <DefaultPieceHasher as Hasher>::Domain {
     let mut buf = [0u8; NODE_SIZE * 2];
     buf[..NODE_SIZE].copy_from_slice(a);
     buf[NODE_SIZE..].copy_from_slice(b);
@@ -344,404 +351,4 @@ pub fn get_aligned_source<T: Read>(
         piece_alignment.clone(),
         with_alignment(source, piece_alignment),
     )
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::api::util::commitment_from_fr;
-    use crate::constants::{DRG_DEGREE, EXP_DEGREE};
-    use crate::types::DataTree;
-
-    use paired::bls12_381::Fr;
-    use rand::{Rng, RngCore, SeedableRng};
-    use rand_xorshift::XorShiftRng;
-    use storage_proofs::drgraph::Graph;
-    use storage_proofs::merkle::create_base_merkle_tree;
-    use storage_proofs::porep::stacked::StackedBucketGraph;
-
-    #[test]
-    fn test_empty_source() {
-        let mut source = EmptySource::new(12);
-        let mut target = Vec::new();
-        source.read_to_end(&mut target).unwrap();
-        assert_eq!(target, vec![0u8; 12]);
-    }
-
-    #[test]
-    fn test_compute_comm_d_empty() {
-        let comm_d = compute_comm_d(SectorSize(2048), &[]).unwrap();
-        assert_eq!(
-            comm_d,
-            [
-                252, 126, 146, 130, 150, 229, 22, 250, 173, 233, 134, 178, 143, 146, 212, 74, 79,
-                36, 185, 53, 72, 82, 35, 55, 106, 121, 144, 39, 188, 24, 248, 51
-            ]
-        );
-
-        let comm_d = compute_comm_d(SectorSize(128), &[]).unwrap();
-        assert_eq!(
-            hex::encode(&comm_d),
-            "3731bb99ac689f66eef5973e4a94da188f4ddcae580724fc6f3fd60dfd488333",
-        );
-    }
-
-    #[test]
-    fn test_get_piece_alignment() {
-        let table = vec![
-            (0, 0, (0, 127)),
-            (0, 127, (0, 0)),
-            (0, 254, (0, 0)),
-            (0, 508, (0, 0)),
-            (0, 1016, (0, 0)),
-            (127, 127, (0, 0)),
-            (127, 254, (127, 0)),
-            (127, 508, (381, 0)),
-            (100, 100, (27, 27)),
-            (200, 200, (54, 54)),
-            (300, 300, (208, 208)),
-        ];
-
-        for (bytes_in_sector, bytes_in_piece, (expected_left_align, expected_right_align)) in
-            table.clone()
-        {
-            let PieceAlignment {
-                left_bytes: UnpaddedBytesAmount(actual_left_align),
-                right_bytes: UnpaddedBytesAmount(actual_right_align),
-            } = get_piece_alignment(
-                UnpaddedBytesAmount(bytes_in_sector),
-                UnpaddedBytesAmount(bytes_in_piece),
-            );
-            assert_eq!(
-                (expected_left_align, expected_right_align),
-                (actual_left_align, actual_right_align)
-            );
-        }
-    }
-
-    #[test]
-    fn test_get_piece_start_byte() {
-        let pieces = [
-            UnpaddedBytesAmount(31),
-            UnpaddedBytesAmount(32),
-            UnpaddedBytesAmount(33),
-        ];
-
-        assert_eq!(
-            get_piece_start_byte(&pieces[..0], pieces[0]),
-            UnpaddedByteIndex(0)
-        );
-        assert_eq!(
-            get_piece_start_byte(&pieces[..1], pieces[1]),
-            UnpaddedByteIndex(127)
-        );
-        assert_eq!(
-            get_piece_start_byte(&pieces[..2], pieces[2]),
-            UnpaddedByteIndex(254)
-        );
-    }
-
-    #[test]
-    fn test_verify_simple_pieces() {
-        let rng = &mut XorShiftRng::from_seed(crate::TEST_SEED);
-
-        //     g
-        //   /  \
-        //  e    f
-        // / \  / \
-        // a  b c  d
-
-        let (a, b, c, d): ([u8; 32], [u8; 32], [u8; 32], [u8; 32]) = rng.gen();
-
-        let mut e = [0u8; 32];
-        let h = piece_hash(&a, &b);
-        e.copy_from_slice(h.as_ref());
-
-        let mut f = [0u8; 32];
-        let h = piece_hash(&c, &d);
-        f.copy_from_slice(h.as_ref());
-
-        let mut g = [0u8; 32];
-        let h = piece_hash(&e, &f);
-        g.copy_from_slice(h.as_ref());
-        let a = PieceInfo::new(a, UnpaddedBytesAmount(127)).unwrap();
-        let b = PieceInfo::new(b, UnpaddedBytesAmount(127)).unwrap();
-        let c = PieceInfo::new(c, UnpaddedBytesAmount(127)).unwrap();
-        let d = PieceInfo::new(d, UnpaddedBytesAmount(127)).unwrap();
-
-        let e = PieceInfo::new(e, UnpaddedBytesAmount(254)).unwrap();
-        let f = PieceInfo::new(f, UnpaddedBytesAmount(254)).unwrap();
-        let g = PieceInfo::new(g, UnpaddedBytesAmount(508)).unwrap();
-
-        let sector_size = SectorSize(4 * 128);
-        let comm_d = g.commitment;
-
-        // println!("e: {:?}", e);
-        // println!("f: {:?}", f);
-        // println!("g: {:?}", g);
-
-        assert!(
-            verify_pieces(
-                &comm_d,
-                &vec![a.clone(), b.clone(), c.clone(), d.clone()],
-                sector_size
-            )
-            .expect("failed to verify"),
-            "[a, b, c, d]"
-        );
-
-        assert!(
-            verify_pieces(&comm_d, &vec![e.clone(), c.clone(), d.clone()], sector_size)
-                .expect("failed to verify"),
-            "[e, c, d]"
-        );
-
-        assert!(
-            verify_pieces(&comm_d, &vec![e.clone(), f.clone()], sector_size)
-                .expect("failed to verify"),
-            "[e, f]"
-        );
-
-        assert!(
-            verify_pieces(&comm_d, &vec![a.clone(), b.clone(), f.clone()], sector_size)
-                .expect("failed to verify"),
-            "[a, b, f]"
-        );
-
-        assert!(
-            verify_pieces(&comm_d, &vec![g], sector_size).expect("failed to verify"),
-            "[g]"
-        );
-    }
-
-    #[test]
-    fn test_verify_padded_pieces() {
-        // [
-        //   {(A0 00) (BB BB)} -> A(1) P(1) P(1) P(1) B(4)
-        //   {(CC 00) (00 00)} -> C(2)      P(1) P(1) P(1) P(1) P(1) P(1)
-        // ]
-        // [
-        //   {(DD DD) (DD DD)} -> D(8)
-        //   {(00 00) (00 00)} -> P(1) P(1) P(1) P(1) P(1) P(1) P(1) P(1)
-        // ]
-
-        let sector_size = SectorSize(32 * 128);
-        let pad = zero_padding(UnpaddedBytesAmount(127)).unwrap();
-
-        let pieces = vec![
-            PieceInfo::new([1u8; 32], UnpaddedBytesAmount(1 * 127)).unwrap(),
-            PieceInfo::new([2u8; 32], UnpaddedBytesAmount(4 * 127)).unwrap(),
-            PieceInfo::new([3u8; 32], UnpaddedBytesAmount(2 * 127)).unwrap(),
-            PieceInfo::new([4u8; 32], UnpaddedBytesAmount(8 * 127)).unwrap(),
-        ];
-
-        let padded_pieces = vec![
-            PieceInfo::new([1u8; 32], UnpaddedBytesAmount(1 * 127)).unwrap(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            PieceInfo::new([2u8; 32], UnpaddedBytesAmount(4 * 127)).unwrap(),
-            PieceInfo::new([3u8; 32], UnpaddedBytesAmount(2 * 127)).unwrap(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            PieceInfo::new([4u8; 32], UnpaddedBytesAmount(8 * 127)).unwrap(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-            pad.clone(),
-        ];
-
-        let hash = |a, b| {
-            let hash = piece_hash(a, b);
-            let mut res = [0u8; 32];
-            res.copy_from_slice(hash.as_ref());
-            res
-        };
-
-        let layer1: Vec<[u8; 32]> = vec![
-            hash(&padded_pieces[0].commitment, &padded_pieces[1].commitment), // 2: H(A(1) | P(1))
-            hash(&padded_pieces[2].commitment, &padded_pieces[3].commitment), // 2: H(P(1) | P(1))
-            padded_pieces[4].commitment,                                      // 4: B(4)
-            padded_pieces[5].commitment,                                      // 2: C(2)
-            hash(&padded_pieces[6].commitment, &padded_pieces[7].commitment), // 2: H(P(1) | P(1))
-            hash(&padded_pieces[8].commitment, &padded_pieces[9].commitment), // 2: H(P(1) | P(1))
-            hash(&padded_pieces[10].commitment, &padded_pieces[11].commitment), // 2: H(P(1) | P(1))
-            padded_pieces[12].commitment,                                     // 8: D(8)
-            hash(&padded_pieces[13].commitment, &padded_pieces[14].commitment), // 2: H(P(1) | P(1))
-            hash(&padded_pieces[15].commitment, &padded_pieces[16].commitment), // 2: H(P(1) | P(1))
-            hash(&padded_pieces[17].commitment, &padded_pieces[18].commitment), // 2: H(P(1) | P(1))
-            hash(&padded_pieces[19].commitment, &padded_pieces[20].commitment), // 2: H(P(1) | P(1))
-        ];
-
-        let layer2: Vec<[u8; 32]> = vec![
-            hash(&layer1[0], &layer1[1]),   // 4
-            layer1[2],                      // 4
-            hash(&layer1[3], &layer1[4]),   // 4
-            hash(&layer1[5], &layer1[6]),   // 4
-            layer1[7],                      // 8
-            hash(&layer1[8], &layer1[9]),   // 4
-            hash(&layer1[10], &layer1[11]), // 4
-        ];
-
-        let layer3 = vec![
-            hash(&layer2[0], &layer2[1]), // 8
-            hash(&layer2[2], &layer2[3]), // 8
-            layer2[4],                    // 8
-            hash(&layer2[5], &layer2[6]), // 8
-        ];
-
-        let layer4 = vec![
-            hash(&layer3[0], &layer3[1]), // 16
-            hash(&layer3[2], &layer3[3]), // 16
-        ];
-
-        let comm_d = hash(&layer4[0], &layer4[1]); // 32
-
-        assert!(verify_pieces(&comm_d, &pieces, sector_size).unwrap());
-    }
-
-    #[ignore] // slow test
-    #[test]
-    fn test_verify_random_pieces() -> Result<()> {
-        use crate::pieces::*;
-
-        let rng = &mut XorShiftRng::from_seed(crate::TEST_SEED);
-
-        for sector_size in &[
-            SectorSize(4 * 128),
-            SectorSize(32 * 128),
-            SectorSize(1024 * 128),
-            SectorSize(1024 * 8 * 128),
-        ] {
-            println!("--- {:?} ---", sector_size);
-            for i in 0..100 {
-                println!(" - {} -", i);
-                let unpadded_sector_size: UnpaddedBytesAmount = sector_size.clone().into();
-                let sector_size = *sector_size;
-                let padded_sector_size: PaddedBytesAmount = sector_size.into();
-
-                let mut piece_sizes = Vec::new();
-                loop {
-                    let sum_piece_sizes: PaddedBytesAmount =
-                        sum_piece_bytes_with_alignment(&piece_sizes).into();
-
-                    if sum_piece_sizes > padded_sector_size {
-                        piece_sizes.pop();
-                        break;
-                    }
-                    if sum_piece_sizes == padded_sector_size {
-                        break;
-                    }
-
-                    'inner: loop {
-                        // pieces must be power of two
-                        let left = u64::from(padded_sector_size) - u64::from(sum_piece_sizes);
-                        let left_power_of_two = prev_power_of_two(left as u32);
-                        let max_exp = (left_power_of_two as f64).log2() as u32;
-
-                        let padded_exp = if max_exp > 7 {
-                            rng.gen_range(
-                                7, // 2**7 == 128,
-                                max_exp,
-                            )
-                        } else {
-                            7
-                        };
-                        let padded_piece_size = 2u64.pow(padded_exp);
-                        let piece_size: UnpaddedBytesAmount =
-                            PaddedBytesAmount(padded_piece_size).into();
-                        piece_sizes.push(piece_size);
-                        let sum: PaddedBytesAmount =
-                            sum_piece_bytes_with_alignment(&piece_sizes).into();
-
-                        if sum > padded_sector_size {
-                            // pieces might be too large after padding, so remove them and try again.
-                            piece_sizes.pop();
-                        } else {
-                            break 'inner;
-                        }
-                    }
-                }
-
-                // println!(
-                //     "  {:?}",
-                //     piece_sizes
-                //         .iter()
-                //         .map(|s| u64::from(*s) / 127)
-                //         .collect::<Vec<_>>()
-                // );
-                assert!(sum_piece_bytes_with_alignment(&piece_sizes) <= unpadded_sector_size);
-                assert!(!piece_sizes.is_empty());
-
-                let (comm_d, piece_infos) = build_sector(&piece_sizes, sector_size)?;
-
-                assert!(
-                    verify_pieces(&comm_d, &piece_infos, sector_size)?,
-                    "invalid pieces"
-                );
-            }
-        }
-
-        Ok(())
-    }
-
-    fn build_sector(
-        piece_sizes: &[UnpaddedBytesAmount],
-        sector_size: SectorSize,
-    ) -> Result<([u8; 32], Vec<PieceInfo>)> {
-        let rng = &mut XorShiftRng::from_seed(crate::TEST_SEED);
-        let porep_id = [32; 32];
-        let graph = StackedBucketGraph::<DefaultPieceHasher>::new_stacked(
-            u64::from(sector_size) as usize / NODE_SIZE,
-            DRG_DEGREE,
-            EXP_DEGREE,
-            porep_id,
-        )?;
-
-        let mut staged_sector = Vec::with_capacity(u64::from(sector_size) as usize);
-        let mut staged_sector_io = std::io::Cursor::new(&mut staged_sector);
-        let mut piece_infos = Vec::with_capacity(piece_sizes.len());
-
-        for (i, piece_size) in piece_sizes.iter().enumerate() {
-            let piece_size_u = u64::from(*piece_size) as usize;
-            let mut piece_bytes = vec![255u8; piece_size_u];
-            rng.fill_bytes(&mut piece_bytes);
-
-            let mut piece_file = std::io::Cursor::new(&mut piece_bytes);
-
-            let (piece_info, _) = crate::api::add_piece(
-                &mut piece_file,
-                &mut staged_sector_io,
-                *piece_size,
-                &piece_sizes[..i],
-            )?;
-
-            piece_infos.push(piece_info);
-        }
-        assert_eq!(staged_sector.len(), u64::from(sector_size) as usize);
-
-        let data_tree: DataTree =
-            create_base_merkle_tree::<DataTree>(None, graph.size(), &staged_sector).unwrap();
-        let comm_d_root: Fr = data_tree.root().into();
-        let comm_d = commitment_from_fr(comm_d_root);
-
-        Ok((comm_d, piece_infos))
-    }
-
-    fn prev_power_of_two(mut x: u32) -> u32 {
-        x |= x >> 1;
-        x |= x >> 2;
-        x |= x >> 4;
-        x |= x >> 8;
-        x |= x >> 16;
-        x - (x >> 1)
-    }
 }
