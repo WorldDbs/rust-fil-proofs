64636c4db1fba6efa36a5c129d94e7cd1e36fc8a
nagydani <nagydani@epointsystem.org>
2021-02-16 12:12:25 +0000

    More precise ivar name.

--- filecoin-proofs/src/types/mod.rs
+++ filecoin-proofs/src/types/mod.rs
@@ -1,8 +1,14 @@
-use serde::{Deserialize, Serialize};
-use storage_proofs::hasher::Hasher;
-use storage_proofs::porep::stacked;
+pub use merkletree::store::StoreConfig;
+pub use storage_proofs_core::merkle::{MerkleProof, MerkleTreeTrait};
+pub use storage_proofs_porep::stacked::{Labels, PersistentAux, TemporaryAux};
 
-use crate::constants::*;
+use filecoin_hashers::Hasher;
+use serde::{Deserialize, Serialize};
+use storage_proofs_core::{merkle::BinaryMerkleTree, sector::SectorId};
+use storage_proofs_porep::stacked;
+use storage_proofs_post::fallback;
+
+use crate::constants::DefaultPieceHasher;
 
 mod bytes_amount;
 mod piece_info;
@@ -10,33 +16,27 @@ mod porep_config;
 mod porep_proof_partitions;
 mod post_config;
 mod post_proof_partitions;
+mod private_replica_info;
+mod public_replica_info;
 mod sector_class;
 mod sector_size;
 
-pub use self::bytes_amount::*;
-pub use self::piece_info::*;
-pub use self::porep_config::*;
-pub use self::porep_proof_partitions::*;
-pub use self::post_config::*;
-pub use self::post_proof_partitions::*;
-pub use self::sector_class::*;
-pub use self::sector_size::*;
+pub use bytes_amount::*;
+pub use piece_info::*;
+pub use porep_config::*;
+pub use porep_proof_partitions::*;
+pub use post_config::*;
+pub use post_proof_partitions::*;
+pub use private_replica_info::*;
+pub use public_replica_info::*;
+pub use sector_class::*;
+pub use sector_size::*;
 
 pub type Commitment = [u8; 32];
 pub type ChallengeSeed = [u8; 32];
-pub use stacked::PersistentAux;
-pub use stacked::TemporaryAux;
 pub type ProverId = [u8; 32];
 pub type Ticket = [u8; 32];
-
-pub type Tree = storage_proofs::merkle::OctMerkleTree<DefaultTreeHasher>;
-pub type LCTree = storage_proofs::merkle::OctLCMerkleTree<DefaultTreeHasher>;
-
-pub use storage_proofs::porep::stacked::Labels;
-pub type DataTree = storage_proofs::merkle::BinaryMerkleTree<DefaultPieceHasher>;
-
-pub use storage_proofs::merkle::MerkleProof;
-pub use storage_proofs::merkle::MerkleTreeTrait;
+pub type DataTree = BinaryMerkleTree<DefaultPieceHasher>;
 
 /// Arity for oct trees, used for comm_r_last.
 pub const OCT_ARITY: usize = 8;
@@ -50,7 +50,7 @@ pub struct SealPreCommitOutput {
     pub comm_d: Commitment,
 }
 
-pub type VanillaSealProof<Tree> = storage_proofs::porep::stacked::Proof<Tree, DefaultPieceHasher>;
+pub type VanillaSealProof<Tree> = stacked::Proof<Tree, DefaultPieceHasher>;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct SealCommitPhase1Output<Tree: MerkleTreeTrait> {
@@ -71,8 +71,6 @@ pub struct SealCommitOutput {
     pub proof: Vec<u8>,
 }
 
-pub use merkletree::store::StoreConfig;
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SealPreCommitPhase1Output<Tree: MerkleTreeTrait> {
     #[serde(bound(
@@ -82,4 +80,21 @@ pub struct SealPreCommitPhase1Output<Tree: MerkleTreeTrait> {
     pub labels: Labels<Tree>,
     pub config: StoreConfig,
     pub comm_d: Commitment,
+}
+
+pub type SnarkProof = Vec<u8>;
+pub type VanillaProof<Tree> = fallback::Proof<<Tree as MerkleTreeTrait>::Proof>;
+
+// This FallbackPoStSectorProof is used during Fallback PoSt, but
+// contains only Vanilla proof information and is not a full Fallback
+// PoSt proof.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct FallbackPoStSectorProof<Tree: MerkleTreeTrait> {
+    pub sector_id: SectorId,
+    pub comm_r: <Tree::Hasher as Hasher>::Domain,
+    #[serde(bound(
+        serialize = "VanillaProof<Tree>: Serialize",
+        deserialize = "VanillaProof<Tree>: Deserialize<'de>"
+    ))]
+    pub vanilla_proof: VanillaProof<Tree>, // Has comm_c, comm_r_last, inclusion_proofs
 }
